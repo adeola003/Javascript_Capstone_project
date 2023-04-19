148578550712f5c220a24a3bf7f101ca59aa76ef
Abdel-Khafid kolawole Fifa SALAOU <119133987+adeola003@users.noreply.github.com>
2023-04-19 09:22:54 +0100

    rmove like increment function

--- src/functions.js
+++ src/functions.js
@@ -25,44 +25,6 @@ const displayMeals = async () => {
   </div>
   <button class="comment-btn" id="${meal.idMeal}" data-target="#popup-wrapper">Comments</button>`;
 
-  //implement likes function
-  const likeButton = document.querySelectorAll('.like-btn');
-  likeButton.forEach((button) => {
-    button.addEventListener('click', async (e) => {
-      const mealId = button.parentElement.parentElement.querySelector('.img-div').id;
-      const url = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${microverseKey}/likes`;
-  
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ item_id: mealId })
-      });
-  
-      if (response.status === 201) {
-        const mealId = e.target.closest('.meals-details').previousElementSibling.dataset.id;
-
-        // Fetch the current likes for the meal from the API
-        const likesResponse = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${microverseKey}/likes`);
-        const likesData = await likesResponse.json();
-        
-        // Find the likes for the current meal ID
-        const mealLikes = likesData.find(like => like.item_id === mealId);
-        
-        // Update the like-count element
-        const likeCountElem = e.target.closest('.meals-details').querySelector('.like-count');
-        if (mealLikes) {
-          likeCountElem.textContent = `${mealLikes.likes} Likes`;
-        } else {
-          likeCountElem.textContent = '0 Likes';
-        }
-        
-      } else {
-        const error = await response.json();
-        console.log(error);
-      }
-  
-    });
-  });
   
 
     // Append the list item element to the unordered list element
@@ -71,3 +33,5 @@ const displayMeals = async () => {
 };
 
 export { displayMeals, mealsURL, getData};
+
+
