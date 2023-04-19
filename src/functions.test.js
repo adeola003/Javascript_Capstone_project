36ca292071a1d30f97bfb46da1420a419ea057d0
citec-47 <mauricendonyi40@gmail.com>
2023-04-19 01:02:26 -0700

    COMMENT AND POPUP

--- src/functions.test.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line
-import { } from './functions';
-
-global.TextEncoder = require('util').TextEncoder;
-global.TextDecoder = require('util').TextDecoder;
-
-// eslint-disable-next-line
-const { JSDOM } = require('jsdom');
