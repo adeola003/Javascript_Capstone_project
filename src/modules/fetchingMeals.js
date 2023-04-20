01f110e6198f140eedcde355214fedf67dcbfd5c
citec-47 <mauricendonyi40@gmail.com>
2023-04-20 07:14:42 -0700

    adding logo

--- src/modules/fetchingMeals.js
+++ src/modules/fetchingMeals.js
@@ -1,9 +1,9 @@
-const URL_API = 'https://www.themealdb.com/api/json/v1/1/search.php?f=b';
+const URL_API = 'https://www.themealdb.com/api/json/v1/1/search.php?f=a';
 
 const getUsFoodList = async (URL_API) => {
   const responses = await fetch(URL_API);
   const json = await responses.json();
-  const mealList = json.meals.slice(0, 18);
+  const mealList = json.meals.slice(0, 52875);
 
   return mealList;
 };
