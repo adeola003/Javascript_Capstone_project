4bea8b56670ca3c4900473c53efbfb0aeb70d802
citec-47 <mauricendonyi40@gmail.com>
2023-04-21 12:04:41 -0700

    pop up comment

---
+++ src/functions.test.js
@@ -0,0 +1,9 @@
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line
+import { } from './functions';
+
+global.TextEncoder = require('util').TextEncoder;
+global.TextDecoder = require('util').TextDecoder;
+
+// eslint-disable-next-line
+const { JSDOM } = require('jsdom');
