0bcde0347c4d9f6d82c979e380a2e137d109dbe8
Abdel-Khafid kolawole Fifa SALAOU <119133987+adeola003@users.noreply.github.com>
2023-04-22 09:44:43 +0100

    fix conflicts

--- src/counts.js
+++ src/counts.js
@@ -1,15 +1,16 @@
 const countItems = () => {
-  const mealsContainer = document.querySelector('.meals-container');
-  const length = mealsContainer.childElementCount;
-  return length;
-};
-
-const countComments = () => {
-
-  const commentsList = document.getElementById('comments-list');
-  const commentCounting = document.querySelector('#comment-counter');
-  const commentsCount = commentsList.childElementCount;
-  commentCounting.innerHTML = `${commentsCount}`;
-};
-
-export { countComments, countItems };
+    const mealsContainer = document.querySelector('.meals-container');
+    const length = mealsContainer.childElementCount;
+    return length;
+  };
+  
+  const countComments = () => {
+    const popup = document.querySelector('.popup-container');
+    const commentsContainer = popup.querySelector('#comments-list');
+    const commentsList = commentsContainer.querySelectorAll('li');
+    const { length } = commentsList;
+    return length;
+  };
+  
+  export { countComments, countItems };
+  
